24f1a73757d7401a1dd55476ef1425775e230eba
Nicolas Maurício Martins Coiado <n.martins@brazilts.local>
2024-02-29 11:59:09 -0300

    Criando meu primeiro programa dotnet.

---
+++ PrimeiroC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeiroC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Olá, mundo";
+            Console.WriteLine(texto);
+        }
+    }
+}
+
