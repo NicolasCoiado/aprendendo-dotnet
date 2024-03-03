72d28de7324c2bef0f44fe55dd06cec3ae71f5e7
Nicolas Coiado <nicolascoiado03@hotmail.com>
2024-03-03 06:06:34 -0300

    Estudando Enums.

---
+++ EnumC/Program.cs
@@ -0,0 +1,34 @@
+﻿Product mouse = new Product(1, "SSD", 400.00, EProductType.Produto);
+
+Console.WriteLine(mouse.Id);
+Console.WriteLine(mouse.Name);
+Console.WriteLine(mouse.Price);
+Console.WriteLine(mouse.Type);
+
+struct Product
+{
+    public Product(int id, string name, double price, EProductType type)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+        Type = type;
+    }
+
+    public int Id;
+    public string Name;
+    public double Price;
+    public EProductType Type;
+
+
+    public double PrinceInDolar(double dolar)
+    {
+        return Price * dolar;
+    }
+}
+
+enum EProductType
+{
+    Produto = 0,
+    Servico = 1
+}
