eb94e6f616596e56f0f505df787da6f575314b11
Nicolas Coiado <nicolascoiado03@hotmail.com>
2024-03-03 05:56:53 -0300

    Estudando Structs.

---
+++ StructsC/Program.cs
@@ -0,0 +1,24 @@
+﻿Product mouse = new Product(1, "SSD", 400.00);
+
+Console.WriteLine(mouse.Id);
+Console.WriteLine(mouse.Name);
+Console.WriteLine(mouse.Price);
+
+struct Product
+{
+    public Product(int id, string name, double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+    public int Id;
+    public string Name;
+    public double Price;
+
+    public double PrinceInDolar(double dolar)
+    {
+        return Price * dolar;
+    }
+}
