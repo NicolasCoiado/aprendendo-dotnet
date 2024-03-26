aac578523c8112acc2b9250602666d4c11b69746
Nicolas Maurício Martins Coiado <n.martins@brazilts.local>
2024-03-26 11:43:27 -0300

    Aprendendo sobre utilização de datas.

---
+++ DatasC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+var aniversario = new DateTime(2003, 12, 03, 14, 30, 10);
+var dataHoje = DateTime.Now;
+var formatada = String.Format("{0:r}", dataHoje);
+
+if (dataHoje == aniversario)
+{
+    Console.WriteLine("Nasceuu");
+}
+else
+{
+    Console.WriteLine("Já nasceu faz tempo :(");
+}
+
+Console.WriteLine(aniversario);
+Console.WriteLine(formatada);
+Console.WriteLine(dataHoje.AddDays(12));
+
+var en = new CultureInfo("en-UK");
+var utcDate = DateTime.UtcNow;
+Console.WriteLine(DateTime.Now.ToString("D", en));
+Console.WriteLine(utcDate);
+Console.WriteLine(utcDate.ToLocalTime());
+
+var timeSpan = new TimeSpan(1);
+Console.WriteLine(timeSpan);
