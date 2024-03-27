a5a66441c1c247da7bf1f3c963f2786db092eafd
Nicolas Maurício Martins Coiado <n.martins@brazilts.local>
2024-03-27 11:11:06 -0300

    Aprendendo sobre tratamento de moedas.

---
+++ MoedasC/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+decimal valor = 18536.25m;
+
+var culture = CultureInfo.CreateSpecificCulture("en-US");
+Console.WriteLine(valor.ToString(culture));
+
+Console.WriteLine(Math.Round(valor));
+Console.WriteLine(Math.Ceiling(valor)); //Arredonda para cima
+Console.WriteLine(Math.Floor(valor)); //Arredonda para baixo
